443a5939c32c544548f52f7638819e7a18409023
chen.keyu <chen.keyu@darcy.com>
2022-11-06 20:26:51 +0800

    feat: 移除内网地址过滤

--- utools/preload/utils/IpUtil.js
+++ utools/preload/utils/IpUtil.js
@@ -4,20 +4,6 @@ function _normalizeFamily(family) {
     return family ? family.toLowerCase() : 'ipv4';
 }
 
-function isPrivate(addr) {
-    return /^(::f{4}:)?10\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})$/i
-            .test(addr) ||
-        /^(::f{4}:)?192\.168\.([0-9]{1,3})\.([0-9]{1,3})$/i.test(addr) ||
-        /^(::f{4}:)?172\.(1[6-9]|2\d|30|31)\.([0-9]{1,3})\.([0-9]{1,3})$/i
-            .test(addr) ||
-        /^(::f{4}:)?127\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})$/i.test(addr) ||
-        /^(::f{4}:)?169\.254\.([0-9]{1,3})\.([0-9]{1,3})$/i.test(addr) ||
-        /^f[cd][0-9a-f]{2}:/i.test(addr) ||
-        /^fe80:/i.test(addr) ||
-        /^::1$/.test(addr) ||
-        /^::$/.test(addr);
-}
-
 function isLoopback(addr) {
     return /^(::f{4}:)?127\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})/
             .test(addr) ||
@@ -44,7 +30,7 @@ let getIpAddresses = function (netInterfaceName) {
 
     return interfaces[netInterfaceName].filter(function (ipAddress) {
         let {family, address, internal} = ipAddress;
-        return family.toLowerCase() === 'ipv4' && !isLoopback(address) && isPrivate(address) && !internal;
+        return family.toLowerCase() === 'ipv4' && !isLoopback(address) && !internal;
     });
 }
 
