1abe481ecd77c499dde1ffee69f19d18dbd672b1
陈科宇 <759891347@qq.com>
2023-07-02 22:12:14 +0800

    feat: 添加ipv6地址显示

--- utools/preload/utils/IpUtil.js
+++ utools/preload/utils/IpUtil.js
@@ -22,7 +22,7 @@ function loopback(family) {
     return family === 'ipv4' ? '127.0.0.1' : 'fe80::1';
 }
 
-let getIpAddresses = function (netInterfaceName) {
+let getIpAddresses = function (netInterfaceName, ipFamily = "ipv4") {
     let interfaces = networkInterfaces();
     if (!interfaces[netInterfaceName]) {
         return [];
@@ -30,29 +30,28 @@ let getIpAddresses = function (netInterfaceName) {
 
     return interfaces[netInterfaceName].filter(function (ipAddress) {
         let {family, address} = ipAddress;
-        return family.toLowerCase() === 'ipv4' && !isLoopback(address);
+        return family.toLowerCase() === ipFamily && !isLoopback(address);
     });
 }
 
-let getNetInterfaceNames = function () {
+let getNetInterfaceNames = function (ipFamily = "ipv4") {
     let interfaces = networkInterfaces();
     return Object.keys(interfaces).filter(function (name) {
         return !/(loopback|vmware|internal|lo|vEthernet)/gi.test(name);
     }).filter(function (name) {
-        return getIpAddresses(name).length > 0;
+        return getIpAddresses(name, ipFamily).length > 0;
     })
 }
 
-let getIpAddress = function (idx = 0) {
-    let names = getNetInterfaceNames();
+let getIpAddress = function (idx = 0, ipFamily = 'ipv4') {
+    let names = getNetInterfaceNames(ipFamily);
     if (!names.length) {
-        return loopback('ipv4');
+        return loopback(ipFamily);
     }
 
     idx = (idx % names.length);
-    let ipAddresses = getIpAddresses(names[idx]);
-
-    return ipAddresses.length > 0 ? ipAddresses[0].address : loopback('ipv4');
+    let ipAddresses = getIpAddresses(names[idx], ipFamily);
+    return ipAddresses.length > 0 ? ipAddresses[0].address : loopback(ipFamily);
 };
 
 exports.getIpAddress = getIpAddress
