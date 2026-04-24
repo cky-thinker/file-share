a160060cd2b2e01e87a6c57362e11980ed11cc43
陈科宇 <759891347@qq.com>
2026-04-24 11:49:39 +0800

    feat: 修复持久层bug

--- utools/preload/utils/PlatformAdapterInstance.js
+++ utools/preload/utils/PlatformAdapterInstance.js
@@ -1,28 +1,48 @@
 const { Database } = require('@file-share/shared-utils');
 const { machineId } = require('node-machine-id')
 
+// 区分不同设备的存储
+let nodeId = null;
+function getMachineId() {
+    if (nodeId == null) {
+        nodeId = machineId(true);
+    }
+    return nodeId;
+}
+
 // 设置数据库适配器
 class UtoolsDatabaseAdapter {
-    setStorageItem(key, value, machineUnique = false) {
-        if (machineUnique) {
-            const id = machineId(true)
-            key = `${key}_${id}`
+    getStorageItem(key, defaultValue = null, machineUnique = true) {
+        if (key.indexOf(":") === -1 && machineUnique) {
+            key = key + ":" + getMachineId();
         }
-        return utools.dbStorage.setItem(key, value)
+        let value = utools.dbStorage.getItem(key);
+        // 存在直接返回
+        if (value !== undefined && value !== null) {
+            return value;
+        }
+        // 不存在且没有默认值
+        if (defaultValue === undefined || defaultValue === null) {
+            return null;
+        }
+        // 使用默认值
+        if (typeof defaultValue === "function") {
+            defaultValue = defaultValue();
+        }
+        self.setStorageItem(key, defaultValue, machineUnique);
+        return defaultValue;
     }
 
-    getStorageItem(key, defaultValue = null, machineUnique = false) {
-        if (machineUnique) {
-            const id = machineId(true)
-            key = `${key}_${id}`
+    setStorageItem(key, value, machineUnique = true) {
+        if (key.indexOf(":") === -1 && machineUnique) {
+            key = key + ":" + getMachineId();
         }
-        return utools.dbStorage.getItem(key) || defaultValue
+        utools.dbStorage.setItem(key, value);
     }
 
-    removeStorageItem(key, machineUnique = false) {
-        if (machineUnique) {
-            const id = machineId(true)
-            key = `${key}_${id}`
+    removeStorageItem(key, machineUnique = true) {
+        if (key.indexOf(":") === -1 && machineUnique) {
+            key = key + ":" + getMachineId();
         }
         return utools.dbStorage.removeItem(key)
     }
